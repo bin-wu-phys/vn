4993b9105203e7310ee8eff5de88a0ba68b3ab5e
Bin Wu <b.wu@cern.ch>
2020-04-09 18:50:40 +0200

    show_F0 is added.   On branch analysis   Changes to be committed: 	modified:   Code/showdata.py

--- Code/showdata.py
+++ Code/showdata.py
@@ -51,50 +51,35 @@ class ShowData:
         """
         Calculate en from the distribution function.
         """
-        # Find out the initial distribution
-        if self.folder == '':
-            fd = self.filename
-        else:
-            fd = self.folder + '/' + self.filename
-
-        files = [f for f in os.listdir(fd) if '.kt.' in f]
-
-        f_init = files[0]
-        for i in range(1, len(files)):
-            if files[i] < f_init:
-                f_init = files[i]
-        f_init = fd + '/' + f_init
-
-        self.kt.load(f_init)
+        # Read F0 if necessary
+        self.read_F0()
 
         if n == 1:
             pn = 3
         else:
             pn = n + 1
-        phi, vz = 0, 0
-        fbg = interpolate.interp1d(self.kt.kern.ob.latt.lattice[0], self.kt.F[0, vz, :, phi], kind='linear',
+
+        fbg = interpolate.interp1d(self.r, self.F0[0], kind='linear',
                                    fill_value=0.0, bounds_error=False)
-        fn = interpolate.interp1d(self.kt.kern.ob.latt.lattice[0], self.kt.F[n, vz, :, phi], kind='linear',
+        fn = interpolate.interp1d(self.r, self.F0[n], kind='linear',
                                   fill_value=0.0, bounds_error=False)
         ebg = integrate.nquad(lambda r: r ** pn * scipy.real(fbg(r)),
-                              [(self.kt.kern.ob.latt.lattice[0][0], self.kt.kern.ob.latt.lattice[0][-1])]
+                              [(self.r[0], self.r[-1])]
                               )[0]
         res = integrate.nquad(lambda r: r ** pn * scipy.real(fn(r)),
-                              [(self.kt.kern.ob.latt.lattice[0][0], self.kt.kern.ob.latt.lattice[0][-1])]
+                              [(self.r[0], self.r[-1])]
                               )[0] + \
               1.0j * integrate.nquad(lambda r: r ** pn * scipy.imag(fn(r)),
-                                     [(self.kt.kern.ob.latt.lattice[0][0], self.kt.kern.ob.latt.lattice[0][-1])]
+                                     [(self.r[0], self.r[-1])]
                                      )[0]
 
-        resnum = np.inner(self.kt.kern.ob.latt.lattice[0] ** pn, self.kt.F[n, vz, :, phi]) / np.inner(
-            self.kt.kern.ob.latt.lattice[0] ** pn, self.kt.F[0, vz, :, phi])
+        resnum = np.inner(self.r ** pn, self.F0[n]) / np.inner(self.r ** pn, self.F0[0])
 
 
         if comment:
-            plt.scatter(self.kt.kern.ob.latt.lattice[0],
-                        self.kt.kern.ob.latt.lattice[0] ** pn * self.kt.F[n, vz, :, phi].real)
-            plt.scatter(self.kt.kern.ob.latt.lattice[0],
-                        self.kt.kern.ob.latt.lattice[0] ** pn * self.kt.F[n, vz, :, phi].imag)
+            plt.scatter(self.r, self.r ** pn * self.F0[n].real)
+            plt.scatter(self.r, self.r ** pn * self.F0[n].imag)
+
             rs = np.arange(0, 3.0, 0.05)
             fintep = fn(rs)
             plt.plot(rs, rs ** pn * fintep.real)
@@ -118,18 +103,70 @@ class ShowData:
         Load all the info from the data
         """
         self.read_vn()
+        self.F0 = None
+        self.r = None
+
+    def read_F0(self):
+        # Find out the initial distribution
+        if self.F0 is None:
+            if self.folder == '':
+                fd = self.filename
+            else:
+                fd = self.folder + '/' + self.filename
+
+            files = [f for f in os.listdir(fd) if '.kt.' in f]
+
+            f_init = files[0]
+            for i in range(1, len(files)):
+                if files[i] < f_init:
+                    f_init = files[i]
+            f_init = fd + '/' + f_init
+
+            phi, vz = 0, 0
+            self.kt.load(f_init)
+            self.r = self.kt.kern.ob.latt.lattice[0]
+            self.F0 = self.kt.F[:, vz, :, phi]
+
+    def show_F0(self, n, ax=None, lw=4, linestyle='-'):
+        """
+        Show F0[n] as a function of r.
+        :param n: integer
+                the harmonic number (in theta).
+        :param ax: an Axies object
+        """
+        # Read F0 if necessary
+        self.read_F0()
+
+        returnQ = False
+        if ax is None:
+            fig, ax = plt.subplots(figsize=(8, 6))
+            returnQ = True
+
+        ax.plot(self.r, self.F0[n].real, linestyle, lw=lw,
+                    label=r'Re $F_{}$'.format(n)
+                )
+        ax.plot(self.r, self.F0[n].imag, linestyle, lw=lw,
+                label=r'Im $F_{}$'.format(n)
+                )
+
+        ax.set_xlabel(r'$r/R$', fontsize=14)
+        ax.legend()
+
+        if returnQ:
+            return fig, ax
 
     def __init__(self, filename='', folder=''):
-
         self.filename = filename
         self.folder = folder
         self.kt = KinTran()
+
+        self.F0 = None
+        self.r = None
 
         if filename != '':
             self.load()
         else:
             self.v = None
-
 
     def show_v(self, n, enQ=True, ax=None, lw=4, label='', linestyle='-'):
         """
